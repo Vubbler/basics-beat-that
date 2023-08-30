b4041abe2d82c1b85aeaccb7e6b033c7c3b4514f
Vubbler <vubbler@gmail.com>
2023-08-30 18:11:01 +0800

    added requirements and problem breakdown

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+// Requirements //
+// 1) There are 2 players and players take turns
+// 2) when a player clicks submit, the game rolls 2 dice and shows the dice rolls, for example 4 and 6
+// 3) the player picks the order of the dice they want, dice 1 or dice 2.
+// 4) after both players have rolled and chosen dice order, the player with the higher combined number wins.
+
+// ===== problem breakdown and planning === //
+// ver 1. rolls 2 dice and returns the output for 1 player. That player chooses the dice order and get the correct return output.
+// ver 2. refactor code to include player 2.
+// ver 3. implement comparing dice scores and declare winner.
+// ver 4. reset the game so that the players can play continually without refreshing the browser page.
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
