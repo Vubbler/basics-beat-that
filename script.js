9368f56bc01ec26538709a306a4b089c33caa893
Vubbler <vubbler@gmail.com>
2023-09-04 19:30:45 +0800

    added player 2

--- script.js
+++ script.js
@@ -10,63 +10,76 @@
 // ver 3. implement comparing dice scores and declare winner.
 // ver 4. reset the game so that the players can play continually without refreshing the browser page.
 
-//create game state variables for 2 steps in the game for one player
-var GAME_STATE_FOR_DICE_ROLL = "GAMESTATE FOR DICE ROLL";
-var GAME_STATE_FOR_DICE_ORDER = "GAME STATE FOR DICE ORDER";
-var gameState = GAME_STATE_FOR_DICE_ROLL;
+// ------Global Variables----- //
+var GAME_STATE_DICE_ROLL = "GAME_STATE_DICE_ROLL";
+var GAME_STATE_CHOOSE_DICE_ORDER = "GAME_STATE_CHOOSE_DICE_ORDER";
+var GAME_STATE_COMPARE_SCORES = "GAME_STATE_COMPARE_SCORES";
+var gameState = GAME_STATE_DICE_ROLL;
 
-//create an array to store player's dice roll values
-var playerRolls = [];
+var currentPlayerRolls = [];
+var currentPlayer = 1;
+var allPlayersScore = [];
 
-//create a helper function to imitate rolling dice
-function rollDie() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-//create a helper function to roll two dice and return a message
-var rollDiceForPlayer = function () {
-  console.log(`Control flow: start of rollDiceForPlayer()`);
-  for (count = 0; count < 2; count += 1) {
-    playerRolls.push(rollDie());
-  }
-
-  console.log(`rollDiceForPlayer changes, playerRolls: `, playerRolls);
-  return `Welcome<br><br>You rolled:<br>Dice 1: ${playerRolls[0]} | Dice 2: ${playerRolls[1]}<br><br>Now please input either "1" or "2" to choose the corresponding dice to be used as the first digit of your final value.`;
+// ------Helper Functions------ //
+var rollDice = function () {
+  var randomDecimal = Math.random() * 6;
+  var randomInteger = Math.floor(randomDecimal) + 1;
+  return randomInteger;
 };
-var main = function (input) {
-  console.log(`Checking game state on submit click: `, gameState);
-  var myOutputMessage = "";
-  if (gameState == GAME_STATE_FOR_DICE_ROLL) {
-    console.log(`Control flow: gameState == GAME_STATE_DICE_ROLL`);
-    //display the dice rolled as output message
-    myOutputMessage = rollDiceForPlayer();
-    //change the game state
-    gameState = GAME_STATE_FOR_DICE_ORDER;
-    return myOutputMessage;
+var rollDiceForPlayer = function () {
+  var counter = 0;
+  while (counter < 2) {
+    currentPlayerRolls.push(rollDice());
+    counter += 1;
   }
-  if (gameState == GAME_STATE_FOR_DICE_ORDER) {
-    console.log(`Control flow: gameState == GAME_STATE_FOR_DICE_ORDER`);
-    //input validation
-    if (input != 1 && input != 2) {
-      console.log(
-        `Control flow: input validation, invalid input ... NOT 1 AND NOT 2`
-      );
-      //return an error message
-      return `Error! Please only input '1' or '2' to choose which dice to use as the first digit.<br><br>Your dice rolls are:<br>Dice 1: ${playerRolls[0]} | Dice 2: ${playerRolls[1]} `;
-    }
-    // input == 1
-    if (input == 1) {
-      console.log(`Control flow: input == 1`);
-      //add dice values by converting to strings, concatenate them, then convert the result back to integers
-      var playerScore = Number(String(playerRolls[0]) + String(playerRolls[1]));
-      return `Your chosen value is: ${playerScore}`;
-    }
+  // if (currentPlayerRolls[0] == currentPlayerRolls[1]) {
+  //   playerScore = Number(
+  //     String(currentPlayerRolls[0]) + String(currentPlayerRolls[1])
+  //   );
+  //   return `Your chosen value is: ${playerScore}`;
+  // }
+  return `Welcome, Player ${currentPlayer}!<br><br>You rolled:<br>Dice 1: ${currentPlayerRolls[0]} | Dice 2: ${currentPlayerRolls[1]}<br><br>Now, please input either '1' or '2' to choose the corresponding dice to be used as the first digit of your final value.`;
+};
+var getPlayerScore = function (playerInput) {
+  var playerScore;
+  if (playerInput != 1 && playerInput != 2) {
+    return `Error! Please only input '1' or '2' to choose which dice to use as the first digit.<br><br>Your dice rolls are:<br>Dice 1: ${currentPlayerRolls[0]} | Dice 2: ${currentPlayerRolls[1]}`;
+  }
+  if (playerInput == 1) {
+    var playerScore = Number(
+      String(currentPlayerRolls[0]) + String(currentPlayerRolls[1])
+    );
+    return `Player ${currentPlayer},your chosen value is: ${playerScore}`;
+  }
+  if (playerInput == 2) {
+    var playerScore = Number(
+      String(currentPlayerRolls[1]) + String(currentPlayerRolls[0])
+    );
+    allPlayersScore.push(playerScore);
+    currentPlayerRolls = [];
+    return `Player ${currentPlayer}, your chosen value is: ${playerScore}`;
+  }
+};
 
-    // input == 2
-    if (input == 2) {
-      console.log(`Control flow: input == 2`);
-      //add dice values by converting to strings, concatenate them, then convert the result back to integers
-      var playerScore = Number(String(playerRolls[1]) + String(playerRolls[0]));
-      return `Your chosen value is: ${playerScore}`;
+// end of helper functions //
+
+var main = function (input) {
+  var outputMessage = "";
+  if (gameState == GAME_STATE_DICE_ROLL) {
+    outputMessage = rollDiceForPlayer();
+    gameState = GAME_STATE_CHOOSE_DICE_ORDER;
+    return outputMessage;
+  }
+  if (gameState == GAME_STATE_CHOOSE_DICE_ORDER) {
+    outputMessage = getPlayerScore(input);
+    if (currentPlayer == 1) {
+      currentPlayer = 2;
+      gameState = GAME_STATE_DICE_ROLL;
+      return `${outputMessage}<br><br>It is now Player 2's turn!`;
     }
+  }
+  if (currentPlayer == 2) {
+    gameState = GAME_STATE_COMPARE_SCORES;
+    return `${outputMessage}<br><br>Press submit to calculate scores!`;
   }
 };
